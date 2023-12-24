fe01620d34266cf19de84fe66c253b803afba094
Olesya <os1124856@gmail.com>
2023-12-24 23:26:22 +0500

    корзина стили

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaCartShopping } from "react-icons/fa6";
 
 export default function Header() {
+  let [openCart, setOpenCart] = useState(false);
   return (
     <header>
       <div>
@@ -10,6 +12,11 @@ export default function Header() {
           <li>Contacts</li>
           <li>Exit</li>
         </ul>
+        <FaCartShopping
+          onClick={() => setOpenCart((openCart = !openCart))}
+          className={`icon-cart ${openCart && "active"}`}
+        />
+        {openCart && <div className="cart-shop">cart-shop</div>}
       </div>
       <div className="background"></div>
     </header>
